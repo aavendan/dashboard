b9e9ec270f9e58e5b36a0c659327424752c83b3f
Allan <aavendan@gmail.com>
2025-07-05 16:40:42 +0000

    guia13

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@
 import './App.css'
 
 import { Grid } from '@mui/material';
+import HeaderUI from './components/HeaderUI';
+import AlertUI from './components/AlertUI';
 
 function App() {
   // const [count, setCount] = useState(0)
@@ -12,22 +14,26 @@ function App() {
     <Grid container spacing={5} justifyContent="center" alignItems="center">
 
       {/* Encabezado */}
-      <Grid size={{ xs: 12, md: 12 }}>Elemento: Encabezado</Grid>
+      <Grid size={{ xs: 12, md: 12 }}>
+        <HeaderUI />
+      </Grid>
 
       {/* Alertas */}
-      <Grid size={{ xs: 12, md: 12 }}>Elemento: Alertas</Grid>
+      <Grid size={{ xs: 12, md: 12 }} container justifyContent="right" alignItems="center">
+        <AlertUI description="No se preveen lluvias"/>
+      </Grid>
 
       {/* Selector */}
-      <Grid size={{ xs: 12, md: 3  }}>Elemento: Selector</Grid>
+      <Grid size={{ xs: 12, md: 3 }}>Elemento: Selector</Grid>
 
       {/* Indicadores */}
       <Grid size={{ xs: 12, md: 9 }}>Elemento: Indicadores</Grid>
 
       {/* Gráfico */}
-      <Grid size={{ xs: 6, md: 6 }} sx={{ display: { xs: "none", md: "block"} }}>Elemento: Gráfico</Grid>
+      <Grid size={{ xs: 6, md: 6 }} sx={{ display: { xs: "none", md: "block" } }}>Elemento: Gráfico</Grid>
 
       {/* Tabla */}
-      <Grid size={{ xs: 6, md: 6 }} sx={{ display: { xs: "none", md: "block"} }}>Elemento: Tabla</Grid>
+      <Grid size={{ xs: 6, md: 6 }} sx={{ display: { xs: "none", md: "block" } }}>Elemento: Tabla</Grid>
 
       {/* Información adicional */}
       <Grid size={{ xs: 12, md: 12 }}>Elemento: Información adicional</Grid>
