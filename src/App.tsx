06317ab6b029d42f5d0f6e247736bd38a65260c2
Allan <aavendan@gmail.com>
2025-11-22 14:14:56 +0000

    guia12

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
 
 import { Grid } from '@mui/material';
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <Grid container spacing={5} justifyContent="center" alignItems="center">
@@ -24,13 +24,13 @@ function App() {
       <Grid size={{xs: 12, md: 9}}>Elemento: Indicadores</Grid>
 
       {/* Gráfico */}
-      <Grid sx={{ display: { xs: "none", md: "block"} }}>Elemento: Gráfico</Grid>
+      <Grid size={{xs: 12, md: 6}} sx={{ display: { xs: "none", md: "block"} }}>Elemento: Gráfico</Grid>
 
       {/* Tabla */}
-      <Grid sx={{ display: { xs: "none", md: "block"} }}>Elemento: Tabla</Grid>
+      <Grid size={{xs: 12, md: 6}} sx={{ display: { xs: "none", md: "block"} }}>Elemento: Tabla</Grid>
 
       {/* Información adicional */}
-      <Grid>Elemento: Información adicional</Grid>
+      <Grid size={12}>Elemento: Información adicional</Grid>
 
     </Grid>
   )
